74e657385b5c014e8fa05bdb0c1bee491cc53e09
Professional <8_00@bk.ru>
2024-05-11 21:07:38 +0300

    Added show_user_menu

--- src/bot/handlers/start.py
+++ src/bot/handlers/start.py
@@ -1,4 +1,3 @@
-import asyncio
 from aiogram import F
 from aiogram import types
 from aiogram import Router
@@ -6,10 +5,8 @@ from aiogram.filters import Command
 from aiogram.filters import CommandStart
 
 from database.requests import create_user, join_to_group, create_event, join_event, get_event_participants, show_photo
-from src import db
 from src.bot.keyboards.reply import main_kb
-from src.bot.keyboards.builders.user import find_kb
-
+from src.bot.keyboards.builders.user import show_user_menu
 
 router = Router()
 
@@ -46,22 +43,15 @@ async def command_join_event(message: types.Message) -> None:
 async def command_meow(message: types.Message) -> None:
     """ Поиск участников по базе. Только тех, кто зарегистрирован на данный ивент. """
     participants = await get_event_participants(message)
-    print(participants)
-    # await message.answer(participants)
-    # users = await db.select.get_users()
-    # await message.answer_photo(photo='AgACAgIAAxkBAAILAWU4BQ4KDDFTIHEB9bY3MjHuWMt5AAJX1jEbY2bBSfT20KWwgEUNAQADAgADeAADMAQ', reply_markup=find_kb(users))
 
 
 @router.message(Command('show_photo'))
 async def command_show_photo(message: types.Message) -> None:
     photo = await show_photo(message)
     await message.answer_photo(photo=photo)
-    # print('dfgdf')
-    # await message.answer_photo(
-    #     photo='AgACAgIAAxkBAAILAWU4BQ4KDDFTIHEB9bY3MjHuWMt5AAJX1jEbY2bBSfT20KWwgEUNAQADAgADeAADMAQ',
-    #     )
 
 
 @router.message(Command('go'))
 async def command_go(message: types.Message) -> None:
-    await message.answer(reply_markup=main_kb, text='fff')
+    photo = await show_photo(message)
+    await message.answer_photo(photo=photo, reply_markup=show_user_menu())
--- src/bot/keyboards/builders/user.py
+++ src/bot/keyboards/builders/user.py
@@ -1,5 +1,5 @@
 from src import db
-from aiogram import types
+from aiogram import types, Bot
 # from aiogram.types import KeyboardButton
 from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
@@ -39,11 +39,15 @@ def find_kb(users) -> types.InlineKeyboardMarkup:
     return builder.as_markup()
 
 
-def show_user(user) -> types.InlineKeyboardMarkup:
-    """ Клавиатура анкеты участника ивента. """
-    builder = InlineKeyboardBuilder()
-    builder.button(text=str(user), callback_data='sdfgsdfg')
-    return builder.as_markup()
+def show_user_menu():
+    buttons = [
+        [
+            types.InlineKeyboardButton(text="Пригласить", callback_data="num_decr"),
+            types.InlineKeyboardButton(text="Пропустить", callback_data="num_incr")
+        ]
+    ]
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
 
 
 def invite(from_user, to_user) -> types.InlineKeyboardMarkup:
